fc38dec4fad5c727b626937b25b0cb5457a893c2
ShahArpanPratikkumar <shaharpan.cg@gmail.commkdir my-frist-repocd my-frist-repogit initecho>
2026-07-07 15:21:41 +0530

    docs(frontend): add JSDoc to React main entry point

---
+++ Frontend/src/main.jsx
@@ -0,0 +1,20 @@
+/**
+ * @file main.jsx
+ * @description React application entry point. Renders root App inside Redux Provider and StrictMode.
+ * @module main
+ */
+
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { Provider } from 'react-redux';
+import { store } from './store';
+import App from './App.jsx';
+import './index.css';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </React.StrictMode>
+);
