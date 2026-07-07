47040722cbd8971a2b3cb1eb728005c489e553ec
ShahArpanPratikkumar <shaharpan.cg@gmail.commkdir my-frist-repocd my-frist-repogit initecho>
2026-07-07 15:18:25 +0530

    chore(constants): add MongoDB collection name constants

---
+++ Backend/constants/dbCollections.js
@@ -0,0 +1,12 @@
+/**
+ * @file dbCollections.js
+ * @description MongoDB collection name constants.
+ *              Prevents typos by centralizing collection name strings.
+ */
+
+const DB_COLLECTIONS = {
+  USERS: 'users',
+  EARTHQUAKES: 'earthquakes',
+};
+
+module.exports = DB_COLLECTIONS;
